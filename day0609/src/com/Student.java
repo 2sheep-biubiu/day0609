7b6eedf6091c29392e6e84418684c95184583fc1
liuyangyang <985408833@qq.com>
2021-06-09 19:24:27 +0800

    更新

--- day0609/src/com/Student.java
+++ day0609/src/com/Student.java
@@ -24,6 +24,14 @@ public class Student {
         this.sex = sex;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public String getName() {
         return name;
     }
